ae756074d2cd57f5c2f8246f87d5e8dd6d6d6c39
heyitsnala <alanwang_2003@hotmail.com>
2021-10-27 12:55:32 -0400

    Updated to include new functions <Items, Scoreboard, Blue/Red side Win/Loss>

--- LeagueMatchHistory.py
+++ LeagueMatchHistory.py
@@ -8,22 +8,31 @@ from datetime import datetime
 import pytz
 import asyncio
 
-discordKey = 'ODk2ODQ5MjQxNzQ0NDg2NDIw.YWNF8Q.6SyS3bLDF5IqUt5azKvhqU9z4Us'
-my_secret1 = 'RGAPI-5ca291bd-bd74-4e3f-b97c-7c6ee3c8d201'
+discordKey = ''
+my_secret1 = ''
 client = discord.Client()
 bot = commands.Bot(command_prefix='.')
+guildIds = ['901567864639725588', '900593820738125855', '900592595120574475', '447868326820184074', '902107837910618142', '902108674431012894', '902033574637752321', '902032500023521301']
+emojiDictionary = {}
+itemEmojiDictionary = {}
+items = {}
+itemsTestStatements = {}
+summonerIndex = range(10)
+itemIndex = range(7)
+print(summonerIndex)
+itemApi = ['item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6']
 
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
 @bot.command()
+
 async def matchhistory(ctx, arg):
-    embed = discord.Embed(
-        title="Title",
-        description="This is a description",
-        colour=discord.Colour.blue()
-    )
+    for gId in guildIds:
+        abc = await bot.fetch_guild(gId)
+        for emote in abc.emojis:
+            emojiDictionary[str(emote.name)] = str(emote.id)
 
     responseSummonerName = requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + arg,
                                         headers={
@@ -58,6 +67,20 @@ async def matchhistory(ctx, arg):
     lastPlayedDate = json.loads(responseLastPlayed.text)['info']['gameEndTimestamp']
     lastMatchType = json.loads(responseLastPlayed.text)['info']['gameMode']
     lastMatchPlayer = json.loads(responseLastPlayed.text)['info']['participants']
+    winLossBlue = json.loads(responseLastPlayed.text)['info']['teams'][0]
+    winLossRed = json.loads(responseLastPlayed.text)['info']['teams'][1]
+
+    def blueSideResult():
+        if winLossBlue['win'] == True:
+            return " <WIN>"
+        elif winLossBlue['win'] == False:
+            return " <LOSS>"
+
+    def redSideResult():
+        if winLossRed['win'] == True:
+            return " <WIN>"
+        elif winLossRed['win'] == False:
+            return " <LOSS>"
     print(str(datetime.fromtimestamp(lastPlayedDate / 1000).strftime("%b %d %y, %H:%M:%S")))
     d = datetime.fromtimestamp(lastPlayedDate / 1000)
     tz = pytz.timezone("UTC")
@@ -67,6 +90,30 @@ async def matchhistory(ctx, arg):
     # timeStamp = str(datetime.fromtimestamp(lastPlayedDate/1000).strftime("%b %d %y, %H:%M:%S"))
     easternTimeStamp = eastern_time.strftime("%b %d %Y")
 
+    itemString = {}
+
+    for i in summonerIndex:
+        itemsTestStatements[str(i)] = [str(lastMatchPlayer[i]['item0']), str(lastMatchPlayer[i]['item1']), str(lastMatchPlayer[i]['item2']),
+        str(lastMatchPlayer[i]['item3']), str(lastMatchPlayer[i]['item4']), str(lastMatchPlayer[i]['item5']), str(lastMatchPlayer[i]['item6'])]
+        for m in itemIndex:
+            if itemsTestStatements[str(i)][m] == '0':
+                itemsTestStatements[str(i)][m] = "bnk"
+            else:
+                continue
+
+    for o in range(10):
+        itemString[str(o)] = [str(discord.utils.get(bot.emojis, name=str(itemsTestStatements[str(o)][0]))),
+        str(discord.utils.get(bot.emojis, name=str(itemsTestStatements[str(o)][1]))),
+        str(discord.utils.get(bot.emojis, name=str(itemsTestStatements[str(o)][2]))),
+        str(discord.utils.get(bot.emojis, name=str(itemsTestStatements[str(o)][3]))),
+        str(discord.utils.get(bot.emojis, name=str(itemsTestStatements[str(o)][4]))),
+        str(discord.utils.get(bot.emojis, name=str(itemsTestStatements[str(o)][5]))),
+        str(discord.utils.get(bot.emojis, name=str(itemsTestStatements[str(o)][6])))]
+        i += 1
+    print(str(discord.utils.get(bot.emojis, name=str(lastMatchPlayer[0]['item1']))))
+    print(itemString)
+    print(itemsTestStatements)
+    print(itemString['0'][1], itemString['0'][2])
     def matchedPuuId():
         matchedPuu = json.loads(responseLastPlayed.text)
         for id in range(0, len(matchedPuu['metadata']['participants'])):
@@ -91,17 +138,30 @@ async def matchhistory(ctx, arg):
     #This will create an emoji of the rank note: name of emoji has to match rank
     gold = discord.utils.get(ctx.guild.emojis, name=soloQueue['tier'].lower())
 
+    embed = discord.Embed(
+        title="",
+        description="",
+        colour=discord.Colour.blue()
+    )
+    # embed = discord.Embed(
+    #     title="Last Game Played",
+    #     description=lastMatchType + "\n" + str(easternTimeStamp),
+    #     colour=discord.Colour.blue()
+    # )
 
     embed.set_footer(text="This is a footer.")
     embed.set_image(url=championIcon)
     embed.set_thumbnail(url="https://static.u.gg/assets/lol/riot_static/11.20.1/img/champion/Xayah.png")
-    embed.set_author(name=summonerName + " " + "Level - " + str(summonerLevel), icon_url=profileIconImage)
-    embed.add_field(name="Blue Side",
-                    value=str(discord.utils.get(ctx.guild.emojis, name=lastMatchPlayer[0]['championName'])) + " " + lastMatchPlayer[0]['summonerName'] + "\n"
-                    + str(discord.utils.get(ctx.guild.emojis, name=lastMatchPlayer[1]['championName'])) + " " + lastMatchPlayer[1]['summonerName'] + "\n"
-                    + str(discord.utils.get(ctx.guild.emojis, name=lastMatchPlayer[2]['championName'])) + " " + lastMatchPlayer[2]['summonerName'] + "\n"
-                    + str(discord.utils.get(ctx.guild.emojis, name=lastMatchPlayer[3]['championName'])) + " " + lastMatchPlayer[3]['summonerName'] + "\n"
-                    + str(discord.utils.get(ctx.guild.emojis, name=lastMatchPlayer[4]['championName'])) + " " + lastMatchPlayer[4]['summonerName'],
+    embed.set_author(name=summonerName + " " + "<Level " + str(summonerLevel) + ">", icon_url=profileIconImage)
+    embed.add_field(name = "Last Game Played",
+                    value = lastMatchType + "\n" + str(easternTimeStamp),
+                    inline=False)
+    embed.add_field(name="Blue Side" + blueSideResult(),
+                    value=str(discord.utils.get(bot.emojis, name=lastMatchPlayer[0]['championName'])) + " " + lastMatchPlayer[0]['summonerName'] + "\n"
+                    + str(discord.utils.get(bot.emojis, name=lastMatchPlayer[1]['championName'])) + " " + lastMatchPlayer[1]['summonerName'] + "\n"
+                    + str(discord.utils.get(bot.emojis, name=lastMatchPlayer[2]['championName'])) + " " + lastMatchPlayer[2]['summonerName'] + "\n"
+                    + str(discord.utils.get(bot.emojis, name=lastMatchPlayer[3]['championName'])) + " " + lastMatchPlayer[3]['summonerName'] + "\n"
+                    + str(discord.utils.get(bot.emojis, name=lastMatchPlayer[4]['championName'])) + " " + lastMatchPlayer[4]['summonerName'],
                     inline=True)
 
     embed.add_field(name="K/D/A",
@@ -112,14 +172,35 @@ async def matchhistory(ctx, arg):
                    + str(lastMatchPlayer[4]['kills']) + "/" + str(lastMatchPlayer[4]['deaths']) + "/" + str(lastMatchPlayer[4]['assists']),
                    inline=True)
 
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
+    embed.add_field(name = "Items",
+                    value = itemString[str(0)][0] + itemString[str(0)][1] +
+                            itemString[str(0)][2] + itemString[str(0)][3] +
+                            itemString[str(0)][4] + itemString[str(0)][5] +
+                            itemString[str(0)][6] + "\n" +
+                            itemString[str(1)][0] + itemString[str(1)][1] +
+                            itemString[str(1)][2] + itemString[str(1)][3] +
+                            itemString[str(1)][4] + itemString[str(1)][5] +
+                            itemString[str(1)][6] + "\n" +
+                            itemString[str(2)][0] + itemString[str(2)][1] +
+                            itemString[str(2)][2] + itemString[str(2)][3] +
+                            itemString[str(2)][4] + itemString[str(2)][5] +
+                            itemString[str(2)][6] + "\n" +
+                            itemString[str(3)][0] + itemString[str(3)][1] +
+                            itemString[str(3)][2] + itemString[str(3)][3] +
+                            itemString[str(3)][4] + itemString[str(3)][5] +
+                            itemString[str(3)][6] + "\n" +
+                            itemString[str(4)][0] + itemString[str(4)][1] +
+                            itemString[str(4)][2] + itemString[str(4)][3] +
+                            itemString[str(4)][4] + itemString[str(4)][5] +
+                            itemString[str(4)][6],
+                    inline = True)
 
-    embed.add_field(name="Red Side",
-                    value=str(discord.utils.get(ctx.guild.emojis, name=lastMatchPlayer[5]['championName'])) + " " + lastMatchPlayer[5]['summonerName'] + "\n"
-                    + str(discord.utils.get(ctx.guild.emojis, name=lastMatchPlayer[6]['championName'])) + " " + lastMatchPlayer[6]['summonerName'] + "\n"
-                    + str(discord.utils.get(ctx.guild.emojis, name=lastMatchPlayer[7]['championName'])) + " " + lastMatchPlayer[7]['summonerName'] + "\n"
-                    + str(discord.utils.get(ctx.guild.emojis, name=lastMatchPlayer[8]['championName'])) + " " + lastMatchPlayer[8]['summonerName'] + "\n"
-                    + str(discord.utils.get(ctx.guild.emojis, name=lastMatchPlayer[9]['championName'])) + " " + lastMatchPlayer[9]['summonerName'],
+    embed.add_field(name="Red Side" + redSideResult(),
+                    value=str(discord.utils.get(bot.emojis, name=lastMatchPlayer[5]['championName'])) + " " + lastMatchPlayer[5]['summonerName'] + "\n"
+                    + str(discord.utils.get(bot.emojis, name=lastMatchPlayer[6]['championName'])) + " " + lastMatchPlayer[6]['summonerName'] + "\n"
+                    + str(discord.utils.get(bot.emojis, name=lastMatchPlayer[7]['championName'])) + " " + lastMatchPlayer[7]['summonerName'] + "\n"
+                    + str(discord.utils.get(bot.emojis, name=lastMatchPlayer[8]['championName'])) + " " + lastMatchPlayer[8]['summonerName'] + "\n"
+                    + str(discord.utils.get(bot.emojis, name=lastMatchPlayer[9]['championName'])) + " " + lastMatchPlayer[9]['summonerName'],
                     inline=True)
 
     embed.add_field(name="K/D/A",
@@ -130,28 +211,43 @@ async def matchhistory(ctx, arg):
                    + str(lastMatchPlayer[9]['kills']) + "/" + str(lastMatchPlayer[9]['deaths']) + "/" + str(lastMatchPlayer[9]['assists']),
                    inline=True)
 
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
+    embed.add_field(name = "Items",
+                    value = itemString[str(5)][0] + itemString[str(5)][1] +
+                            itemString[str(5)][2] + itemString[str(5)][3] +
+                            itemString[str(5)][4] + itemString[str(5)][5] +
+                            itemString[str(5)][6] + "\n" +
+                            itemString[str(6)][0] + itemString[str(6)][1] +
+                            itemString[str(6)][2] + itemString[str(6)][3] +
+                            itemString[str(6)][4] + itemString[str(6)][5] +
+                            itemString[str(6)][6] + "\n" +
+                            itemString[str(7)][0] + itemString[str(7)][1] +
+                            itemString[str(7)][2] + itemString[str(7)][3] +
+                            itemString[str(7)][4] + itemString[str(7)][5] +
+                            itemString[str(7)][6] + "\n" +
+                            itemString[str(8)][0] + itemString[str(8)][1] +
+                            itemString[str(8)][2] + itemString[str(8)][3] +
+                            itemString[str(8)][4] + itemString[str(8)][5] +
+                            itemString[str(8)][6] + "\n" +
+                            itemString[str(9)][0] + itemString[str(9)][1] +
+                            itemString[str(9)][2] + itemString[str(9)][3] +
+                            itemString[str(9)][4] + itemString[str(9)][5] +
+                            itemString[str(9)][6],
+                    inline = True)
 
-    embed.add_field(name="Last Game Played", value=str(easternTimeStamp) + "\n" + lastMatchType, inline=False)
-
-    # embed.add_field(name= "Test", value = "test\ntest\ntest", inline = True)
-
-    f = discord.guild.Guild.emojis
-    print(f)
     await ctx.send(embed=embed)
-    msg = await ctx.send(f"<:JarvanIV:900593440620961852>")
-    await msg.add_reaction("<:JarvanIV:900593440620961852>")
-
-    checkmark = str(discord.utils.get(bot.emojis, name='Xayah'))
-    await ctx.send(checkmark)
 
 
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('.'):
-        emoji = discord.utils.get(bot.emojis, name='Sejuani')
-        await message.channel.send(str(emoji))
 
+# @bot.command()
+# async def getGuildId(ctx):
+#     await ctx.send(ctx.message.guild.id)
+
+# @bot.command()
+# async def getEmojiDictionary1(ctx):
+#     for gId in guildIds:
+#         abc = await bot.fetch_guild(gId)
+#         for emote in abc.emojis:
+#             #bot.fetch_guild(gId).emoji[emote]
+#             print(":" + str(emote.name) + ":" + str(emote.id))
 
 bot.run(discordKey)
